39627624975c37ec9f47e6d723f0c7da695d3926
Amy <amaule65@gmail.com>
2024-04-18 18:13:56 +0200

    Add image to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ const Header = () => {
   return (
     <div className="header-container">
       <div className="header-title">Chameleon Kitchen</div>
+      <img src="/chef-chameleon.png" className="header-img" />
     </div>
   )
 }
