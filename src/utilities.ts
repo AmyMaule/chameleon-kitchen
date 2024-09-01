8618651d83fbf5a5caef2461c77297df815d2eab
Amy <amaule65@gmail.com>
2024-09-01 10:57:51 +0200

    Fix bug with fractions, small refactor

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,12 +1,12 @@
 // Ensure eggs are correctly converted and things like 'eggplant' are not included in egg conversion
-export const isEgg = (recipeLine: string) => {
+export const isEgg = (ingredient: string) => {
   const eggVariants = ["egg", "eggs", "egg white", "egg whites", "egg yolk", "egg yolks"];
-  return eggVariants.includes(recipeLine?.toLowerCase());
+  return eggVariants.includes(ingredient?.toLowerCase());
 }
 
-export const isSpoonMeasure = (recipeLine: string) => {
-  const spoonMeasureVariants = ["tsp", "tbsp"];
-  return spoonMeasureVariants.includes(recipeLine?.toLowerCase());
+export const isSpoonMeasure = (unit: string) => {
+  const spoonMeasureVariants = ["tsp", "tbsp", "teaspoon", "tablespoon"];
+  return spoonMeasureVariants.includes(unit?.toLowerCase());
 }
 
 export const fractionUnits = ["cup", "cups", "tsp", "teaspoon", "teaspoons", "tbsp", "tablespoon", "tablespoons"];
@@ -14,7 +14,7 @@ export const fractionUnits = ["cup", "cups", "tsp", "teaspoon", "teaspoons", "tb
   // use Euclidean algorithm to get highest common factor of numerator and denominator
 function highestCommonFactor(numerator: number, denominator: number) {
   while (denominator !== 0) {
-    let remainder = numerator % denominator;
+    const remainder = numerator % denominator;
     numerator = denominator;
     denominator = remainder;
   }
@@ -32,22 +32,29 @@ export const decimalToFraction = (amount: number, unit: string = "cup") => {
   const fractionToNearestThird = Math.round((amount - integer) * 3) / 3;
 
   // find out whether the nearest eighth or third is closest to the decimal input
-  const closestFraction = Math.abs(fractionToNearestThird - amount) < Math.abs(fractionToNearestEighth - amount)
+  const decimal = Math.abs(amount - integer);
+  const closestFraction = Math.abs(fractionToNearestThird - decimal) < Math.abs(fractionToNearestEighth - decimal)
     ? fractionToNearestThird
     : fractionToNearestEighth
 
-  // if amount is an integer, return without further calculations
-  if (closestFraction === 0) {
-    return integer > 1 ? `${integer} ${unit}s` : `${integer} ${unit}`;
+  // if amount is an int or the fraction is closer to an int than to a fraction (e.g. 2.95 tsp should be 3 tsp not 2 7/8 tsp)
+  // round to nearest whole number without further calculations
+  if (closestFraction === 0 || closestFraction === 1) {
+    return integer === 1
+      ? `${Math.round(amount)} ${unit}` 
+      : `${Math.round(amount)} ${unit}s`;
   }
 
   let denominator = closestFraction === fractionToNearestEighth ? 8 : 3;
   let numerator = Math.round(closestFraction * denominator);
 
-  let divisor = highestCommonFactor(numerator, denominator);
+  const divisor = highestCommonFactor(numerator, denominator);
   numerator /= divisor;
   denominator /= divisor;
 
-  let fraction = integer ? `${integer} ${numerator}/${denominator}` : `${numerator}/${denominator}`;
-  return integer > 1 ? `${fraction} ${unit}s` : `${fraction} ${unit}`;
+  const fraction = integer ? `${integer} ${numerator}/${denominator}` : `${numerator}/${denominator}`;
+
+  return integer <= 1
+    ? `${fraction} ${unit}` 
+    : `${fraction} ${unit}s`;
 }
